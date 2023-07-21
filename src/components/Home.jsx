ef8d90ec0b80e5dbf1d60a7fce8b5738fd7f9b55
MuhammadSubhan98 <subhan@deltalabs.io>
2023-07-22 00:38:22 +0500

    Initial integration

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,22 +1,35 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
 export default function Home() {
+
   const homeStyles = {
     marginTop: '5rem',
   };
 
+  useEffect(() => {
+    axios
+    .get("http://localhost:3000/api/v1/users/1")
+    .then((res) => 
+      console.log(res)
+      );
+  }, []);
+
   return (
-    <div style={homeStyles}>
-      <Link to="/TicTacToe">
-        <button>TicTacToe</button>
-      </Link>
-      <Link to="/RockPaperScissor">
-        <button>RockPaperScissor</button>
-      </Link>
-      <Link to="/TowerOfHanoi">
-        <button>TowerOfHanoi</button>
-      </Link>
-    </div>
+    <>
+
+      <div style={homeStyles}>
+        <Link to="/TicTacToe">
+          <button>TicTacToe</button>
+        </Link>
+        <Link to="/RockPaperScissor">
+          <button>RockPaperScissor</button>
+        </Link>
+        <Link to="/TowerOfHanoi">
+          <button>TowerOfHanoi</button>
+        </Link>
+      </div>
+    </>
   );
 }
